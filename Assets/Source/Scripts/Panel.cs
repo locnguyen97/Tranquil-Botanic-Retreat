8d48b79f2baaae9842d6d6ee7b7038d3914a7d2c
locnguyen97 <46517710+locnguyen97@users.noreply.github.com>
2023-09-30 17:05:06 +0700

    0

---
+++ Assets/Source/Scripts/Panel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour
+{
+    [SerializeField] private GameObject real;
+    /*[SerializeField] private GameObject fake;*/
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        real.SetActive(false);
+    }
+
+    public void Show()
+    {
+        real.SetActive(true);
+    }
+}
